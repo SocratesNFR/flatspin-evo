7945ccc8f3576e2f6075f9eb3daad3a71c9b21a0
arthurpenty <arthur.penty@ntnu.no>
2024-03-20 14:12:22 +0100

    syntax error

--- fitness_functions.py
+++ fitness_functions.py
@@ -1149,7 +1149,7 @@ def constant_activity_fitness(pop, gen, outdir, active_state=1, state_step=None,
         kwargs = {k: flatspin_kwargs[k] for k in ("size", "hc") if k in flatspin_kwargs} # get the size and hc from flatspin_kwargs if they exist
         asi = model_obj(**kwargs)
         
-        hc = np.ones(flatspin_kwargs.get("size", (4, 4))
+        hc = np.ones(flatspin_kwargs.get("size", (4, 4)))
         
         hc[[0, -1], :] = 100
         hc[:, [0, -1]] = 100
