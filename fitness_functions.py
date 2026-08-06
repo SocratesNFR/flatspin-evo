e4df4bfeab3a141af7bae324046d8916105783c1
arthur penty <arthurpenty@googlemail.com>
2026-08-06 10:16:11 +0200

    proliferate fitness: forgot to delete old code in last commit

--- fitness_functions.py
+++ fitness_functions.py
@@ -433,7 +433,6 @@ def proliferate_fitness(pop, gen, outdir, t_start=7, t_end=-1, luckyknot=False,t
 
         out_comps, counts = np.unique(end, return_counts=True)
         out_comps = out_comps[counts >= min_domain_size]
-        out_comps = set(np.unique(end))
 
         n_exclusive_out = len(out_comps - in_comps)
         n_out = len(out_comps) - 1 # subtract 1 for the background component
